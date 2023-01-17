8846e8d6d72350640d4e36696ea64b286d304966
PythonCoderAS <13932583+PythonCoderAS@users.noreply.github.com>
2023-01-17 14:40:33 -0500

    Dump code

--- src/constants.py
+++ src/constants.py
@@ -1,6 +1,15 @@
-from wikidata_bot_framework import session
-
 mix_n_match_prop = "P2264"
 num_records = "P4876"
 point_in_time = "P585"
-download_url = "https://mix-n-match.toolforge.org/api.php?query=download2&catalogs={catalog_id}&columns=%7B%22exturl%22%3A0%2C%22username%22%3A0%2C%22aux%22%3A0%2C%22dates%22%3A0%2C%22location%22%3A0%2C%22multimatch%22%3A0%7D&hidden=%7B%22any_matched%22%3A0%2C%22firmly_matched%22%3A0%2C%22user_matched%22%3A0%2C%22unmatched%22%3A0%2C%22automatched%22%3A0%2C%22name_date_matched%22%3A0%2C%22aux_matched%22%3A0%2C%22no_multiple%22%3A0%7D&format=json"
+reference_url = "P854"
+stated_in = "P248"
+mix_n_match_item = "Q28054658"
+start_time_prop = "P580"
+earliest_date_prop = "P1319"
+latest_start_time_prop = "P8555"
+latest_date = "P1326"
+deactivated_catalog = "Q116215886"
+reason_for_deprecation = "P2241"
+reason_for_preferred = "P7452"
+most_recent = "Q71533355"
+api_url = "https://mix-n-match.toolforge.org/api.php?query=single_catalog&catalog_id={catalog_id}"
--- src/main.py
+++ src/main.py
@@ -1,47 +1,202 @@
-import pywikibot
-
 import datetime
+import re
 from typing import Union
 
-from wikidata_bot_framework import PropertyAdderBot, site, ExtraProperty, ExtraQualifier, ExtraReference, get_sparql_query, OutputHelper
-from .constants import mix_n_match_prop, num_records, download_url, point_in_time, session
+import pywikibot
+from wikidata_bot_framework import (
+    EntityPage,
+    ExtraProperty,
+    ExtraQualifier,
+    ExtraReference,
+    OutputHelper,
+    PropertyAdderBot,
+    get_random_hex,
+    get_sparql_query,
+    session,
+    site,
+)
+from wikidata_bot_framework.dataclasses import WikidataReference
+
+from .constants import *
+
 
 class MixNMatchBot(PropertyAdderBot):
     def __init__(self):
+        super().__init__()
         self.data = get_sparql_query(mix_n_match_prop)
 
-
     def get_edit_group_id(self):
-        return "e568dc18d70cbb77808b91a712f6e612"
+        return get_random_hex()
 
     def get_edit_summary(self, _):
         return "Updating Mix'n'Match information"
 
-    def should_process_item(self, item: Union[pywikibot.ItemPage, pywikibot.PropertyPage, pywikibot.LexemePage]):
-        if 
+    def get_newest_claim(
+        self,
+        item: Union[pywikibot.ItemPage, pywikibot.PropertyPage, pywikibot.LexemePage],
+    ) -> Union[pywikibot.Claim, None]:
+        newest_claim: Union[pywikibot.Claim, None] = None
+        newest_time: Union[pywikibot.Timestamp, None] = None
+        claims = item.claims.get(num_records, [])
+        for claim in claims:
+            claim: pywikibot.Claim
+            if qualifiers := claim.qualifiers.get(point_in_time, []):
+                if target := qualifiers[0].getTarget():
+                    target: pywikibot.WbTime
+                    if not newest_time or target.toTimestamp() > newest_time:
+                        newest_time = target.toTimestamp()
+                        newest_claim = claim
+        if newest_claim is None and claims:
+            for claim in claims:
+                if claim.rank == "preferred":
+                    newest_claim = claim
+                    break
+            else:
+                newest_claim = claims[0]
+        return newest_claim
 
+    def same_main_property(
+        self,
+        existing_claim: pywikibot.Claim,
+        new_claim: pywikibot.Claim,
+        page: EntityPage,
+    ) -> bool:
+        if existing_claim.id != num_records:
+            return super().same_main_property(existing_claim, new_claim, page)
+        newest_claim = self.get_newest_claim(page)
+        return super().same_main_property(existing_claim, new_claim, page) and (
+            existing_claim == newest_claim or existing_claim.rank == "preferred"
+        )
 
-    def run_item(self, item: Union[pywikibot.ItemPage, pywikibot.PropertyPage, pywikibot.LexemePage]):
-        oh = OutputHelper()
-        mix_n_match_id = self.data[item.id]
-        r = session.get(download_url.format(catalog_id=mix_n_match_id))
-        r.raise_for_status()
-        data = r.json()
-        count = len(data)
+    def now(self):
         now_ts = pywikibot.Timestamp.now(datetime.timezone.utc)
-        now = pywikibot.WbTime(year=now_ts.year, month=now_ts.month, day=now_ts.day)
+        return pywikibot.WbTime(year=now_ts.year, month=now_ts.month, day=now_ts.day)
+
+    def reference(self, item: EntityPage) -> ExtraReference:
+        ref = ExtraReference(
+            url_match_pattern=re.compile(
+                r"https://mix-n-match\.toolforge\.org/#/catalog/\d+"
+            )
+        )
+        claim = pywikibot.Claim(site, stated_in)
+        claim.setTarget(pywikibot.ItemPage(site, mix_n_match_item))
+        ref.add_claim(claim, also_match_property_values=True)
+        claim = pywikibot.Claim(site, reference_url)
+        claim.setTarget(
+            f"https://mix-n-match.toolforge.org/#/catalog/{self.data[item.id].copy().pop()}"
+        )
+        ref.add_claim(claim)
         claim = pywikibot.Claim(site, mix_n_match_prop)
-        claim.setTarget(mix_n_match_id)
-        extra_property = ExtraProperty(claim)
-        qual_1 = pywikibot.Claim(site, point_in_time)
-        qual_1.setTarget(now)
-        extra_property.add_qualifier(ExtraQualifier(qual_1, replace_if_conflicting_exists=True, delete_other_if_replacing=True))
-        qual_2 = pywikibot.Claim(site, num_records)
-        qual_2.setTarget(pywikibot.WbQuantity(count, site=site))
-        extra_property.add_qualifier(ExtraQualifier(qual_2, replace_if_conflicting_exists=True, delete_other_if_replacing=True))
-        oh.add_property(extra_property)
+        claim.setTarget(self.data[item.id].copy().pop())
+        ref.add_claim(claim)
+        return ref
+
+    def item_has_different_source(self, item: EntityPage) -> bool:
+        ref = self.reference(item)
+        for claim in item.claims.get(num_records, []):
+            claim: pywikibot.Claim
+            if claim.sources:
+                for source in claim.sources:
+                    source: WikidataReference
+                    if not ref.is_compatible_reference(source):
+                        return True
+        return False
+
+    def run_item(self, item: EntityPage):
+        oh = OutputHelper()
+        mix_n_match_ids = self.data[item.id]
+        multiple = len(mix_n_match_ids) > 1
+        for mix_n_match_id in mix_n_match_ids:
+            r = session.get(api_url.format(catalog_id=mix_n_match_id))
+            r.raise_for_status()
+            data = r.json()
+            count = data["data"][mix_n_match_id]["total"]
+            active = data["data"][mix_n_match_id]["active"] == "1"
+            earliest_match = data["data"][mix_n_match_id]["earliest_match"]
+            latest_match = data["data"][mix_n_match_id]["latest_match"]
+            if earliest_match:
+                start = pywikibot.Timestamp.strptime(earliest_match, "%Y%m%d%H%M%S")
+                start_time = pywikibot.WbTime(
+                    year=start.year, month=start.month, day=start.day
+                )
+            if latest_match:
+                end = pywikibot.Timestamp.strptime(latest_match, "%Y%m%d%H%M%S")
+                end_time = pywikibot.WbTime(year=end.year, month=end.month, day=end.day)
+            now = self.now()
+            claim = pywikibot.Claim(site, mix_n_match_prop)
+            claim.setTarget(mix_n_match_id)
+            extra_property = ExtraProperty(claim)
+            current_qualifiers = item.claims[mix_n_match_prop][0].qualifiers
+            if (
+                start_time_prop in current_qualifiers
+                or earliest_date_prop in current_qualifiers
+            ):
+                pass
+            else:
+                if earliest_match:
+                    qual_1 = pywikibot.Claim(site, latest_start_time_prop)
+                    qual_1.setTarget(start_time)
+                    extra_property.add_qualifier(
+                        ExtraQualifier(qual_1, replace_if_conflicting_exists=True)
+                    )
+            if not active:
+                if latest_match:
+                    qual_2 = pywikibot.Claim(site, latest_date)
+                    qual_2.setTarget(end_time)
+                    extra_property.add_qualifier(
+                        ExtraQualifier(qual_2, replace_if_conflicting_exists=True)
+                    )
+                claim.setRank("deprecated")
+                qual_3 = pywikibot.Claim(site, reason_for_deprecation)
+                qual_3.setTarget(pywikibot.ItemPage(site, deactivated_catalog))
+                extra_property.add_qualifier(
+                    ExtraQualifier(qual_3, replace_if_conflicting_exists=True)
+                )
+            oh.add_property(extra_property)
+            if multiple:
+                continue
+            claim = pywikibot.Claim(site, num_records)
+            claim.setTarget(pywikibot.WbQuantity(count, site=site))
+            claim.setRank("preferred")
+            extra_property = ExtraProperty(claim)
+            extra_property.add_reference(self.reference(item))
+            qual_1 = pywikibot.Claim(site, point_in_time)
+            qual_1.setTarget(now)
+            qual_2 = pywikibot.Claim(site, reason_for_preferred)
+            qual_2.setTarget(pywikibot.ItemPage(site, most_recent))
+            most_recent_claim = self.get_newest_claim(item)
+            extra_property.add_qualifier(
+                ExtraQualifier(qual_1, skip_if_conflicting_exists=True)
+            )
+            extra_property.add_qualifier(
+                ExtraQualifier(qual_2, skip_if_conflicting_exists=True)
+            )
+            if (
+                (
+                    most_recent_claim
+                    and most_recent_claim.getTarget().amount < int(count)
+                )
+                or not most_recent_claim
+            ) and not self.item_has_different_source(item):
+                oh.add_property(extra_property)
         return oh
-    
+
+    def pre_edit_process_hook(
+        self, output: dict[str, ExtraProperty], item: EntityPage
+    ) -> None:
+        if num_records not in output:
+            return
+        for claim in item.claims.get(num_records, []):
+            claim: pywikibot.Claim
+            if qualifiers := claim.qualifiers.get(point_in_time, []):
+                if self.now().toTimestamp() - qualifiers[
+                    0
+                ].getTarget().toTimestamp() < datetime.timedelta(days=1):
+                    continue
+            if claim.rank == "preferred":
+                claim.setRank("normal")
+                if reason_for_preferred in claim.qualifiers:
+                    del claim.qualifiers[reason_for_preferred]
 
     def run(self):
         self.feed_items([site.get_entity_for_entity_id(id) for id in self.data.keys()])
